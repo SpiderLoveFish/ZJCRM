1788f87bab190d01ad0b3974b89ddbf92d444fec
SPIDER <spider0015@hotmail.com>
2015-12-21 10:12:27 +0800

    UPDATE BY SPDIER

--- CRM/Data/CRM_CEStage.ashx.cs
+++ CRM/Data/CRM_CEStage.ashx.cs
@@ -119,8 +119,13 @@ namespace XHD.CRM.Data
                 string serchtxt = "1=1";
                serchtxt +=" and id not in(SELECT CustomerID FROM dbo.CRM_CEStage) ";
                if (!string.IsNullOrEmpty(request["company"]))
-                   serchtxt += " and Customer like N'%" + PageValidate.InputText(request["company"], 255) + "%'";
+               {
+                   serchtxt += " and ( Customer like '%" + PageValidate.InputText(request["company"], 255) + "%'";
+                   serchtxt += " OR address like '%" + PageValidate.InputText(request["company"], 255) + "%'";
+                   serchtxt += " OR tel like '%" + PageValidate.InputText(request["company"], 255) + "%'";
+                   serchtxt += " OR Emp_sg like '%" + PageValidate.InputText(request["company"], 255) + "%' )";
 
+               }
 
                 string dt = "";
 
