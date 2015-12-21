a2c56811ceb6ebd9ab5b96f1eaa791032c50fa55
SPIDER <spider0015@hotmail.com>
2015-12-21 16:52:51 +0800

    jsopoc测试

---
+++ CRM/test/Default.aspx.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Data;
+using System.Configuration;
+using System.Collections;
+using System.Web;
+using System.Web.Security;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Web.UI.WebControls.WebParts;
+using System.Web.UI.HtmlControls;
+
+namespace Spider.Web
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+             var cmd = Request["Action"];
+             if (cmd == "form")
+             {
+                 try
+                 {
+                     //DataRow[] dr = Caches.CRM_Repair_Follow.Select("RepairID=" + Request["rid"] + " AND IsDel='N'");
+                     //string jdata = Tools.DataRowToJson(dr, Types.JosnType.Grid);
+                     string jdata = "{\"TEST1\":[{\"name\":\"自定义1\",\"x\":445,\"y\":153},{\"name\":\"自定义2\",\"x\":355,\"y\":151}],";
+                     jdata += " \"TEST2\":[{\"name\":\"zz1\",\"x\":0,\"y\":1} ]}";
+                     Response.ContentType = "application/json";
+                     Response.Write(jdata);
+                 }
+                 catch (Exception ex)
+                 {
+                     Response.Write(ex.Message);
+                 }
+                 finally
+                 {
+                     Response.End();
+                 }
+             }
+        }
+    }
+}
