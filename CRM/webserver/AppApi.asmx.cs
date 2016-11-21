b4adb923f0d585cdc2cd7ec4a322dee4e22e18eb
Spider <spider00153@hotmail.com>
2016-11-21 09:00:09 +0800

    11

--- CRM/webserver/AppApi.asmx.cs
+++ CRM/webserver/AppApi.asmx.cs
@@ -247,9 +247,8 @@ namespace XHD.CRM.webserver
               sb.AppendLine("SELECT * FROM dbo.App_Version WHERE	Ver!='" + ver + "'  and IsLast='Y'");
               if (type == "ipad")
                   sb.AppendLine(" and VerStyle='ipad'");
-              else if (type == "ipad")
+              else if (type == "iphone")
                   sb.AppendLine(" and VerStyle='iphone'");
-              
               DataSet ds = DbHelperSQL.Query(sb.ToString(), parameters);
               if (ds == null)
               {
@@ -699,6 +698,8 @@ namespace XHD.CRM.webserver
               if (RV > 0)
                   ReturnStr(true, "\"success\"");
               else ReturnStr(false, "\"faile\"");
+              push("1", "  ", title, context);
+              pushpad("3", "  ", title, context);
 
 
           }
@@ -1033,7 +1034,7 @@ namespace XHD.CRM.webserver
           [WebMethod]
           public void pushapple(string deviceToken)
           {
-              string aa = push("1", "  ", "测试标题", "：测试内容");
+              string aa = push("1", "  ", "测试标题", "：测试内容") + pushpad("3", "  ", "测试标题", "：测试内容");
 
               ReturnStr(true, aa);
 
@@ -1667,7 +1668,7 @@ namespace XHD.CRM.webserver
          }
 
         /// <summary>
-        /// 
+        /// 手机推送
         /// </summary>
         /// <param name="lx">发送类型</param>
          private string push(string lx,string ClientWhere,string Title_add,string body_add)
@@ -1694,23 +1695,105 @@ namespace XHD.CRM.webserver
               DataSet dsclient = DbHelperSQL.Query(sqlclient, parameters);
               PushMessage pm = new PushMessage();
               string iosresult = "";
-              foreach (DataRow dw in dsclient.Tables[0].Select(" Versions='ios'"))
+              foreach (DataRow dw in dsclient.Tables[0].Select(" Versions='ios' OR Versions='ios/phone'"))
               {
                iosclient+=";"+dw["clientid"].ToString();
        
               }
-              iosresult=  pm.apnPush(HOST, APPKEY, MASTERSECRET, APPID, iosclient,Title,BODY);
+              iosresult = pm.apnPush(HOST, APPKEY, MASTERSECRET, APPID, iosclient, Title, StringTruncat(BODY,50,"..."));
               string android = "";
-              foreach (DataRow dw in dsclient.Tables[0].Select(" Versions='android'"))
+              foreach (DataRow dw in dsclient.Tables[0].Select(" Versions='android' OR Versions='ios/phone'"))
               {
                 androidclient+=";"+dw["clientid"].ToString();
             
                  }
-              android = pm.PushMessageToList(HOST, APPKEY, MASTERSECRET, APPID, androidclient, Title, BODY);
-             return iosresult + "<=>" + android;
+              android = pm.PushMessageToList(HOST, APPKEY, MASTERSECRET, APPID, androidclient, Title, StringTruncat(BODY, 50, "..."));
+           
+              return iosresult + "<=>" + android  ;
+
+         }
+         /// <summary>
+         /// PAD推送
+         /// </summary>
+         /// <param name="lx">发送类型</param>
+         private string pushpad(string lx, string ClientWhere, string Title_add, string body_add)
+         {
+             string SQL = "SELECT * FROM dbo.App_PushSetting  where id=" + lx;
+             SqlParameter[] parameters = { };
+             DataSet ds = DbHelperSQL.Query(SQL, parameters);
+             string HOST = ""; string APPKEY = ""; string MASTERSECRET = ""; string APPID = "";
+             string iosclient = "";//苹果
+             string androidclient = "";//安卓
+             string Title = ""; string BODY = "";
+             foreach (DataRow dw in ds.Tables[0].Rows)
+             {
+                 HOST = dw["HOST"].ToString();
+                 APPKEY = dw["APPKEY"].ToString();
+                 MASTERSECRET = dw["MASTERSECRET"].ToString();
+                 APPID = dw["APPID"].ToString();
+                 Title = dw["NotyTitle"].ToString() + Title_add;
+                 BODY = dw["NotyText"].ToString() + body_add;
+             }
+             string sqlclient = "SELECT * FROM  dbo.APP_PushClient WHERE 1=1 ";
+             if (ClientWhere != "")
+                 sqlclient += ClientWhere;
+             DataSet dsclient = DbHelperSQL.Query(sqlclient, parameters);
+             PushMessage pm = new PushMessage();
+           
+             string iospad = "";
+             foreach (DataRow dw in dsclient.Tables[0].Select(" Versions='ios/pad'"))
+             {
+                 iosclient += ";" + dw["clientid"].ToString();
+
+             }
+             iospad = pm.apnPush(HOST, APPKEY, MASTERSECRET, APPID, iosclient, Title, StringTruncat(BODY, 50, "..."));
+             string androidpad = "";
+             foreach (DataRow dw in dsclient.Tables[0].Select("   Versions='android/pad'"))
+             {
+                 androidclient += ";" + dw["clientid"].ToString();
+
+             }
+             androidpad = pm.PushMessageToList(HOST, APPKEY, MASTERSECRET, APPID, androidclient, Title, StringTruncat(BODY, 50, "..."));
+
+             return   iospad  + "<=>" + androidpad;
 
          }
 
+
+         ///   <summary> 
+         ///   将指定字符串按指定长度进行截取并加上指定的后缀
+         ///   </summary> 
+         ///   <param   name= "oldStr "> 需要截断的字符串 </param> 
+         ///   <param   name= "maxLength "> 字符串的最大长度 </param> 
+         ///   <param   name= "endWith "> 超过长度的后缀 </param> 
+         ///   <returns> 如果超过长度，返回截断后的新字符串加上后缀，否则，返回原字符串 </returns> 
+         public static string StringTruncat(string oldStr, int maxLength, string endWith)
+         {
+             //判断原字符串是否为空
+             if (string.IsNullOrEmpty(oldStr))
+                 return oldStr + endWith;
+
+
+             //返回字符串的长度必须大于1
+             if (maxLength < 1)
+                 return "";
+
+
+             //判断原字符串是否大于最大长度
+             if (oldStr.Length > maxLength)
+             {
+                 //截取原字符串
+                 string strTmp = oldStr.Substring(0, maxLength);
+
+
+                 //判断后缀是否为空
+                 if (string.IsNullOrEmpty(endWith))
+                     return strTmp;
+                 else
+                     return strTmp + endWith;
+             }
+             return oldStr;
+         } 
 
         /// <summary>
         /// 客户权限
